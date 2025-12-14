fb329dbf0968f98827b894a8299c7702ec2a349f
orekiousun <1187749429@qq.com>
2025-12-14 19:49:15 +0800

    93 2 learn

--- Source/Aura/Private/UI/HUD/AuraHUD.cpp
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -4,6 +4,7 @@
 #include "UI/HUD/AuraHUD.h"
 #include "Blueprint/UserWidget.h"
 #include "UI/Widget/AuraUserWidget.h"
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
 #include "UI/WidgetController/OverlayWidgetController.h"
 
 UOverlayWidgetController* AAuraHUD::GetOverlayWidgetController(const FWidgetControllerParams& WCParams)
@@ -17,10 +18,23 @@ UOverlayWidgetController* AAuraHUD::GetOverlayWidgetController(const FWidgetCont
 	return OverlayWidgetController;
 }
 
+UAttributeMenuWidgetController* AAuraHUD::GetAttributeMenuWidgetController(const FWidgetControllerParams& WCParams)
+{
+	if (AttributeMenuWidgetController == nullptr)
+	{
+		AttributeMenuWidgetController = NewObject<UAttributeMenuWidgetController>(
+			this, AttributeMenuWidgetControllerClass);
+		AttributeMenuWidgetController->SetWidgetControllerParams(WCParams);
+		AttributeMenuWidgetController->BindCallbacksToDependencies();
+	}
+	return AttributeMenuWidgetController;
+}
+
 void AAuraHUD::InitOverlay(APlayerController* PC, APlayerState* PS, UAbilitySystemComponent* ASC, UAttributeSet* AS)
 {
 	checkf(OverlayWidgetClass, TEXT("Overlay Widget Class uninitialized, please fill out BP_AuraHUD"));
-	checkf(OverlayWidgetControllerClass, TEXT("Overlay Widget Controller Class uninitialized, please fill out BP_AuraHUD"));
+	checkf(OverlayWidgetControllerClass,
+	       TEXT("Overlay Widget Controller Class uninitialized, please fill out BP_AuraHUD"));
 	UUserWidget* Widget = CreateWidget<UUserWidget>(GetWorld(), OverlayWidgetClass);
 	OverlayWidget = Cast<UAuraUserWidget>(Widget);
 	const FWidgetControllerParams WCParams(PC, PS, ASC, AS);
---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,23 @@
+// Copyright ousun
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+#include "AuraGameplayTags.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "AbilitySystem/Data/AttributeInfo.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+
+}
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+	UAuraAttributeSet* AS = CastChecked<UAuraAttributeSet>(AttributeSet);
+	check(AttributeInfo);
+	
+	FAuraAttributeInfo Info = AttributeInfo->FindAttributeInfoForTag(FAuraGameplayTags::Get().Attributes_Primary_Strength);
+	Info.AttributeValue = AS->GetStrength();
+	AttributeInfoDelegate.Broadcast(Info);
+}
